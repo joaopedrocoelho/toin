55c0b235eac2e7353ec0b4a7ac8f51542f10b109
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-10-18 22:08:51 +0800

    removed dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import "./App.css";
-import { CircularLinkedList } from "./classes/CircularLinkedList";
 import Board from "./components/board/Board";
 
 function App() {
-  const List = CircularLinkedList.fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  List.print();
-
   return (
     <>
       <div className="flex w-full h-screen items-center justify-center">
