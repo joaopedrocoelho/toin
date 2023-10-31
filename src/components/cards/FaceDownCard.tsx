886f977882e11a73e93dde23406f91b8bc8cc29f
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-10-31 19:19:07 +0800

    added stroke to face card template

--- src/components/cards/FaceDownCard.tsx
+++ src/components/cards/FaceDownCard.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const FaceDownCard = () => {
   return (
-    <div className="flex text-white items-center justify-center aspect-[8/11] bg-purple-950 rounded-md min-w-[200px]">
+    <div
+      className="flex text-white items-center 
+    justify-center aspect-[8/11] bg-purple-950 
+    rounded-md min-w-[200px]"
+      style={{
+        boxShadow: "0px 0px 0px 4px #ff9200",
+      }}
+    >
       Face Down Card
     </div>
   );
