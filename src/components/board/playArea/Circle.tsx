9ea57b0324f624c0c78b3181521ee1718e95d6f1
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-25 22:29:37 +0800

    added numbers for debug in board

--- src/components/board/playArea/Circle.tsx
+++ src/components/board/playArea/Circle.tsx
@@ -8,6 +8,22 @@ interface CircleProps {
 }
 
 const Circle = ({ circleArr, onClick }: CircleProps) => {
+  const getValueColor = (value: number) => {
+    switch (value) {
+      case 1:
+        return "text-amber-400";
+      case 2:
+        return "text-cyan-200";
+      case 3:
+        return "text-rose-600";
+      case 4:
+        return "text-lime-300";
+      case 5:
+        return "text-zinc-300";
+      default:
+        return "text-white";
+    }
+  };
   return (
     <div
       className="circle border-[0.25px] border-white/30"
@@ -23,11 +39,14 @@ const Circle = ({ circleArr, onClick }: CircleProps) => {
               fill: "white",
             }}
           >
-            {value === 1 && <Star className="fill-amber-400" />}
+            {/* {value === 1 && <Star className="fill-amber-400" />}
             {value === 2 && <Moon className=" fill-cyan-200" />}
             {value === 3 && <Fire className=" fill-rose-600" />}
             {value === 4 && <Thunder className=" fill-lime-300" />}
-            {value === 5 && <Clover className=" fill-zinc-300" />}
+            {value === 5 && <Clover className=" fill-zinc-300" />} */}
+            <h1 className={`text-4xl font-bold ${getValueColor(value)}`}>
+              {value}
+            </h1>
           </div>
         );
       })}
